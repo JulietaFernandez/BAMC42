221640c3b951935eeef0a9e49f9ac6c067605c9f
JulietaFernandez <julietafernandezn@gmail.com>
2022-11-29 20:33:39 -0300

    agregando comandos

---
+++ Clase 02/comandos.js
@@ -0,0 +1,16 @@
+/*
+1=> Para añadir todos los cambios
+git add.
+
+2=> Para confirmar los cambios
+git commit -m "texto descriptivo"
+
+3=> Para elevar al repositorio remoto en GIT HUB
+git push
+
+4=> Para saber el estado de los archivos
+git status
+(este comando podemos hacerlo en cualquier momento y varias veces)
+
+
+*/
